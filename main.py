5bce29d15ac3911a1196ad971ad2a4ee729470b2
medwuu <aepegon@gmail.com>
2023-08-06 22:52:53 +0300

    добавлена возможность найстройки скрипта

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import json
 import string
 import random
-import dotenv
 import requests
+import configparser
 from tqdm import tqdm
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -12,70 +12,86 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
+
 def init():
     options = Options()
-    options.binary_location = os.getenv("BROWSER_PATH")
-    # TODO: в конце, чтобы окно не открывалось
-    # options.add_argument("--headless")
+    options.binary_location = config['PATH']['BROWSER_PATH']
+    if not config['WINDOW']['IS_VISIBLE']:
+        options.add_argument('--headless')
     options.page_load_strategy = 'eager'
     global browser
     browser = webdriver.Firefox(options=options)
     browser.set_window_position(0, 0)
-    browser.set_window_size(1500, 700)
+    # аргумент set_window_size позволяет передавать str значения, поэтому здесь нет преобразования в int
+    browser.set_window_size(config['WINDOW']['WIDTH'], config['WINDOW']['HEIGHT'])
     
 
-def getImgLinks(img_count):
+def getImgLinks():
     letters = string.ascii_lowercase + string.digits
     links_list = {}
-    for iter in tqdm(range(img_count)):
+    for iter in tqdm(range(int(config['WORK']['IMG_COUNT']))):
         postfix = ''.join([random.choice(letters) for _ in range(6)])
-        link = f"https://prnt.sc/{postfix}"
+        link = f'https://prnt.sc/{postfix}'
         browser.get(link)
         # Позволяет приступить к проверке фото сразу после того, как на странице полностью загрузится <body>
-        WebDriverWait(browser, 10).until(expected_conditions.visibility_of_element_located((By.TAG_NAME, "body")))
+        WebDriverWait(browser, 10).until(expected_conditions.visibility_of_element_located((By.TAG_NAME, 'body')))
         img_link = checkImg(browser.page_source)
         if not img_link:
             continue
         
-        if saveImg(img_link, postfix):
+        if int(config['WORK']['USE_FAST_METHOD']) or saveImg(img_link, postfix):
             links_list[postfix] = img_link
 
 
-        if iter % int(os.getenv("BACKUP_COUNT")) == 0:
+        if iter % int(config['WORK']['BACKUP_COUNT']) == 0:
             writeIntoJSON(links_list)
     writeIntoJSON(links_list)
 
 
-def writeIntoJSON(links_list=[], filename="links"):
-    with open(f"{filename}.json", "w") as f:
+def writeIntoJSON(links_list):
+    with open(f'{config["PATH"]["JSON_NAME"]}.json', 'w') as f:
         json.dump(links_list, f, indent=4, ensure_ascii=False)
 
 def checkImg(html):
-    soup = BeautifulSoup(html, "lxml")
-    img_section = soup.find("img", id="screenshot-image")
-    if img_section and not img_section.has_attr("attempt"):
-        img_link = img_section.get_attribute_list("src")[0]
-        if not "//st.prntscr.com" in img_link:
+    soup = BeautifulSoup(html, 'lxml')
+    img_section = soup.find('img', id='screenshot-image')
+    if img_section and not img_section.has_attr('attempt'):
+        img_link = img_section.get_attribute_list('src')[0]
+        if not '//st.prntscr.com' in img_link:
             return img_link
-    return ""
+    return ''
     
 
 def saveImg(url, postfix):
     r = requests.get(url)
-    if r.url != "https://i.imgur.com/removed.png" and \
-    "404 Not Found" not in r.text:
-        with open(f"result\\{postfix}.png", "wb") as f:
+    if r.url != 'https://i.imgur.com/removed.png' and \
+    '404 Not Found' not in r.text:
+        with open(f'{config["PATH"]["RESULT_FOLDER"]}\\{postfix}.png', 'wb') as f:
             f.write(r.content)
             return True
     return False
 
+def saveImgFast():
+    with open(f'{config["PATH"]["JSON_NAME"]}.json', 'r') as f:
+        file_links = json.load(f)
+    postfixes = list(file_links.keys())
+    links = list(file_links.values())
+    for x in tqdm(range(len(links))):
+        saveImg(links[x], postfixes[x])
+
 
 
 def main():
-    dotenv.load_dotenv()
+    global config
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    input("Включите VPN и нажмите Enter...")
     init()
-    getImgLinks(int(os.getenv("IMG_COUNT")))
+    getImgLinks()
     browser.quit()
+    if int(config['WORK']['USE_FAST_METHOD']):
+        input("Для скачивания картинок отключите VPN и нажмите Enter...")
+        saveImgFast()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
